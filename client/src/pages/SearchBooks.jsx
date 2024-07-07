fe5b8ffdff1d4be270311a43baf57968fecc6f5b
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-07-06 21:26:34 -0400

    finished searchbooks.jsx conversion.

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -34,6 +34,7 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -45,11 +46,12 @@ const SearchBooks = () => {
     event.preventDefault();
 
     if (!searchInput) {
+      console.log('No Input!');
       return false;
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -81,13 +83,16 @@ const SearchBooks = () => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
+
+      console.log('user not logged in!');
+
       return false;
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({ variables: { input: bookToSave }}); // FIXED: saveBook not defined. 
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error('something went wrong!');
       }
 
