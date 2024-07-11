2343d4d1c1bce6bab0b7223db58be8024a1f33e8
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-07-11 13:13:38 -0400

    me and my friends atlas and render

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ChrisVulpine:99ctQeKKE0pS3ccI@cluster0.6rugocc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ChrisVulpine:99ctQeKKE0pS3ccI@cluster0.6rugocc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 
 module.exports = mongoose.connection;
